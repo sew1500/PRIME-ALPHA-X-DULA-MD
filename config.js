4965eb2a7402d80116fe6799cc484b841dd6bdeb
𝙿𝚁𝙸𝙼𝙴 𝙰𝙻𝙿𝙷𝙰-𝙼𝙳 𝚅 01.0 <primealpha727@gmail.com>
2025-08-13 20:53:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "your sesson id",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dula9x/PRIME-ALPHA-X-DULA-MD/blob/main/images/PRIME%20ALPHA%20X%20DULA%20MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*I'am Alive Now!! PRIME-ALPHA-X-DULA-MD 🤭💗 ආහ් පැටියෝ කොහොමද ?🌝*",
+ALIVE_MSG: process.env.ALIVE_MSG || "* 👋 Hellow I'am Alive Now!! DARK NOVA XMD V1.0.0 simple wa user bot",
 BOT_OWNER: '94752978237/94770349867', 
 
 
