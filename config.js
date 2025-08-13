4cd964fc394918d1dba0a875c3d744f6cee1152f
PRIME-ALPHA-X-DULA-MD <dulinanethmira626@gmail.com>
2025-08-13 19:59:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nCBS2YBI#QNquJpLlFXfPc_lfSxcOU_LRDW6Z0VPmy6gXKQBavLQ",
+SESSION_ID: process.env.SESSION_ID || "your sesson id",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dula9x/PRIME-ALPHA-X-DULA-MD/blob/main/images/PRIME%20ALPHA%20X%20DULA%20MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*I'am Alive Now!! PRIME-ALPHA-X-DULA-MD 🤭💗 ආහ් පැටියෝ කොහොමද ?🌝*",
 BOT_OWNER: '94752978237/94770349867', 
